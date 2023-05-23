2daf073a2d9861363a0fa4068f78a979a5980d89
Vincentius Andri Kurnianto <jkt48newfans@gmail.com>
2023-05-23 18:49:30 +0700

    Update jeketi.py

--- jeketi.py
+++ jeketi.py
@@ -9,20 +9,25 @@ def search_item(list, product):
             return True
     return False
 
-list_members=["OFC","Shani","Gracia","Feni","Sisca","Gita","Jesslyn","Eli","Chika","Olla","Zee","Muthe","Christy","Jessi","Freya","Fiony","Oniel","Lulu","Flora","Adel","Kathrina","Ashel","Marsha","Indah","Lyn","Lia","Indira","Amanda","Callie","Ella","Raisha","Cynthia","Daisy","Danella","Anindya","Gracie","Greesel","Alya","Jeane","Michie","Elin","Chelsea","Cathy","Gendhis"]
-list_id=[332503,318207,318208,317738,317739,318117,318229,318118,318209,318222,317727,318204,318112,318228,318225,318223,318218,318232,318224,318239,318230,318210,318233,318227,400717,400713,400716,400710,400714,400715,400718,461463,461465,461466,461452,416478,416479,461451,461480,461481,461475,461458,461454,461476]
+list_members = ["OFC","Shani","Gracia","Feni","Sisca","Gita","Jesslyn","Eli","Chika","Olla","Zee","Muthe","Christy","Jessi","Freya","Fiony","Oniel","Lulu","Flora","Adel","Kathrina","Ashel","Marsha","Indah","Lyn","Lia","Indira","Amanda","Callie","Ella","Raisha","Cynthia","Daisy","Danella","Anindya","Gracie","Greesel","Alya","Jeane","Michie","Elin","Chelsea","Cathy","Gendhis"]
+
+list_id = [332503,318207,318208,317738,317739,318117,318229,318118,318209,318222,317727,318204,318112,318228,318225,318223,318218,318232,318224,318239,318230,318210,318233,318227,400717,400713,400716,400710,400714,400715,400718,461463,461465,461466,461452,416478,416479,461451,461480,461481,461475,461458,461454,461476]
 
 print('Selamat Datang di Bot Rebornian48 - Showroom JKT48')
+print(' ')
 print('Di sini akan menampilkan data terkait showroom member JKT48')
+print(' ')      
 print('Silakan input "list" untuk menampilkan data terkait kata kunci, serta input "nama member" yang terdapat pada kunci untuk menampilkan data terkait showroom member JKT48')
+
 x = input()
+
 if search_item(list_members, x):
     num = list_id[list_members.index(x)]
     api_url = "https://www.showroom-live.com/api/room/profile?room_id="+str(num)
-
     response = requests.get(api_url)
     res = response.json()
-    print(datetime.datetime.now().strftime("%d %b %Y %H:%M:%S"))
+    waktu = datetime.datetime.now().strftime("%d %b %Y %H:%M:%S")
+    print("Data per : " + waktu)
     if res['is_onlive']==True:
         if res['premium_room_type']==1:
             pre="premium"
@@ -45,22 +50,31 @@ if search_item(list_members, x):
             list_name.append(response2[i]['name'])
             list_point.append(response2[i]['point'])
             i += 1
-
         x = np.array(list_name)
         y = np.array(list_point)
-
-        plt.title("Penonton " + str(res['room_name']), loc = 'center')
+        plt.title("Penonton " + str(res['room_name']) + "\n" + "Data per : " + waktu, loc = 'center')
         plt.xlabel("Poin")
         plt.barh(x,y)
-
         for i, v in enumerate(y):
             plt.text(v + 1, i, str(v), color='blue', va='center')
-
         plt.show()
     print(' ')
     print('Saat ini jumlah follower ' + res['room_name'] + ' sebanyak ' + f"{res['follower_num']:,d}")
+
 if x=="list":
     for y in list_members:
         print(y)
-else:
-    print("Member yang bernama " + x + " tidak ditemukan!")
+if x=="live":
+    num=0
+    for y in list_id:
+        api_url="https://www.showroom-live.com/api/room/profile?room_id=" + str(y)
+        response = requests.get(api_url)
+        res = response.json()
+        stat="";
+        if res["is_onlive"]==True:
+            stat="Live"
+        else:
+            stat="Offline"
+        print("Status " + res['room_name'] + " : " + stat)
+    print("Jumlah member yang live : " + str(num))
+    print("Data jumlah member yang live beserta daftar namanya akan segera hadir")
